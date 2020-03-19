636569ca74e66d34c1512b4d0b001b4a8d71b400
lihui <www15119258@qq.com>
2020-03-19 17:04:26 +0800

    lesson2 commit

--- src/main/java/com/cangzhitao/springboot/study/HelloWorldController.java
+++ src/main/java/com/cangzhitao/springboot/study/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.cangzhitao.springboot.study;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/")
@@ -12,5 +13,10 @@ public class HelloWorldController {
 	public Object helloWorld() {
 		return "Hello world";
 	}
-
+	
+	@GetMapping(value = "test")
+	public ModelAndView test() {
+		return new ModelAndView("test");
+	}
+	
 }
