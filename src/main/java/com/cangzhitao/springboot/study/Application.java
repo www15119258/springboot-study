c4d2911b1dfad2529627e026f63a6f5a4325c2ff
lihui <www15119258@qq.com>
2020-03-20 10:01:59 +0800

    lesson04

--- src/main/java/com/cangzhitao/springboot/study/Application.java
+++ src/main/java/com/cangzhitao/springboot/study/Application.java
@@ -2,8 +2,12 @@ package com.cangzhitao.springboot.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = { "com.cangzhitao" })
+@EnableJpaRepositories(basePackages = { "com.cangzhitao" })
 public class Application {
 
 	public static void main(String[] args) {
